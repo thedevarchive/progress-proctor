ae39d194e5e0d8d87eaebdd7a1cfc87e0e1695d3
Alyssa Ngo <amaengo@protonmail.com>
2025-04-07 14:00:36 +1000

    Added API for toggling module completion, and a function for updating progress depending on number of modules and how many of them are completed

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const User = require('../models/User'); 
+const User = require('../models/User');
 const authenticateToken = require("../middleware/auth");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
@@ -13,7 +13,7 @@ router.post("/courses", authenticateToken, async (req, res) => {
   const { title } = req.body;
 
   try {
-    const user = await User.findById(req.user.userId); 
+    const user = await User.findById(req.user.userId);
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
@@ -33,7 +33,7 @@ router.post("/courses", authenticateToken, async (req, res) => {
 
 router.get("/courses/:id", authenticateToken, async (req, res) => {
   try {
-    const user = await User.findById(req.user.userId); 
+    const user = await User.findById(req.user.userId);
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
@@ -97,20 +97,34 @@ router.delete("/courses/:id", authenticateToken, async (req, res) => {
 
     user.courses = user.courses.filter(course => course._id.toString() !== req.params.id);
 
-    await user.save(); 
+    await user.save();
 
-    res.status(200).json({ message: "Course deleted"});
+    res.status(200).json({ message: "Course deleted" });
   } catch (err) {
     console.error("Error deleting course:", err);
     res.status(500).json({ message: "Server error" });
   }
 });
 
+async function updateProgress(course) {
+  const completedCount = course.modules.filter(m => m.isFinished).length;
+  const moduleCount = course.modules.length;
+
+  if(moduleCount === 0 || completedCount === 0) {
+    course.progress = 0; 
+  }
+  else {
+    course.progress = Math.round((completedCount / moduleCount) * 100); 
+  }
+
+  console.log(completedCount, moduleCount, course.progress); 
+}
+
 router.post("/courses/:id/modules", authenticateToken, async (req, res) => {
   const { title } = req.body;
 
   try {
-    const user = await User.findById(req.user.userId); 
+    const user = await User.findById(req.user.userId);
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
@@ -122,13 +136,52 @@ router.post("/courses/:id/modules", authenticateToken, async (req, res) => {
       return res.status(404).json({ message: "Course not found" });
     }
 
-    course.modules.push({ title }); 
+    course.modules.push({ title });
 
     await user.save(); // Save changes to DB
 
-    res.status(201).json({ message: "Module added successfully", modules: course.modules });
+    await updateProgress(course); // update course progress
+    await user.save(); // Save changes to progress after 
+
+    res.status(201).json({ message: "Module added successfully", progress: course.progress, modules: course.modules });
   } catch (err) {
-    console.error("Error adding course:", err);
+    console.error("Error adding module:", err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.put("/courses/:c_id/modules/:m_id/finished", authenticateToken, async (req, res) => {
+  console.log(req.params);
+  try {
+    const user = await User.findById(req.user.userId); // find user (top level)
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const course = user.courses.find(c => c.id === req.params.c_id); // find course (array inside top level schema User)
+
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+
+    const module = course.modules.find(c => c.id === req.params.m_id); // find module (not top level so also use find)
+
+    if (!module) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+
+    // Modify completion status of module
+    module.isFinished = !module.isFinished;
+
+    await user.save(); // Save changes to the user's document
+
+    await updateProgress(course); // update course progress
+    await user.save(); // Save changes to progress after 
+
+    res.status(200).json({ message: "Module completion validated", progress: course.progress }); //only return course to update progress bar
+  } catch (err) {
+    console.error("Error editing module:", err);
     res.status(500).json({ message: "Server error" });
   }
 });
